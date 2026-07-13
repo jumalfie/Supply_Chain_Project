397b471af62030728d72128a83dd2dfa31c9f311
Alfred Mutua <mutuaalfred0@gmail.com>
2026-07-13 08:29:14 -0600

    Updated table data type

--- 01_create_tables_dw.sql
+++ 01_create_tables_dw.sql
@@ -17,17 +17,17 @@ CREATE TABLE dim_customers (
 
 -- Product dimensional tables 
 CREATE TABLE dim_products (
-    product_name    VARCHAR     PRIMARY KEY,
-    product_id      INTEGER,
-    category        VARCHAR, 
-    price_INR       INTEGER, 
-    price_USD       INTEGER,
+    product_id        INTEGER     PRIMARY KEY,
+    product_name      VARCHAR,
+    category          VARCHAR, 
+    price_INR         DECIMAL (10,2), 
+    price_USD         DECIMAL (10,2)
 ); 
 
 
 -- Target Orders dimensional table
 CREATE TABLE dim_target_orders (
-    customer_id                 INTEGER,
+    customer_id                 INTEGER     PRIMARY KEY,
     ontime_target_percent       INTEGER,
     infull_target_percent       INTEGER,
     otif_target_percent         INTEGER,
@@ -37,13 +37,12 @@ CREATE TABLE dim_target_orders (
 
 -- Fact Aggregate table
 CREATE TABLE fact_aggregate (
-    order_id                VARCHAR,
+    order_id                VARCHAR     PRIMARY KEY,
     customer_id             INTEGER,
     order_placement_date    DATE,
     on_time                 VARCHAR,
     in_full                 VARCHAR,
     otif                    VARCHAR,
-    PRIMARY KEY (order_id),
     FOREIGN KEY (customer_id) REFERENCES dim_customers (customer_id)
 );
 
@@ -60,9 +59,11 @@ CREATE TABLE fact_order_line (
     in_full                 VARCHAR,
     on_time                 VARCHAR,
     otif                    VARCHAR,
-    FOREIGN KEY(order_id) REFERENCES fact_aggregate(order_id),
-    FOREIGN KEY(customer_id) REFERENCES dim_customers(customer_id),
-    );
+PRIMARY KEY(order_id,product_id),
+FOREIGN KEY (order_id) REFERENCES fact_aggregate(order_id),
+FOREIGN KEY (customer_id) REFERENCES dim_customers(customer_id),
+FOREIGN KEY (product_id) REFERENCES dim_products(product_id)
+);
 
 SELECT table_name
 FROM information_schema.tables 
